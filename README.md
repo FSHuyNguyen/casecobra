6e7fdfe689d007428494c9f902e43060867c8aa7
Nguyễn Văn Huy <huynv@overate-vntech.com>
2024-05-21 09:45:56 +0700

    initial commit

--- README.md
+++ README.md
@@ -34,3 +34,19 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+
+## Library
+- UI shadcn
+- lucide-react
+- kinde
+- framer-motion
+- react-dropzone
+- uploadthing
+- zod
+- neon
+- sharp
+- react-rnd
+- headlessui
+- react-query
+- react-dom-confetti
+- stripe
---
+++ src/app/auth-callback/page.tsx
@@ -0,0 +1,54 @@
+"use client";
+import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import { getAuthStatus } from "./actions";
+import { useRouter } from "next/navigation";
+import { Loader2 } from "lucide-react";
+
+const Page = () => {
+    const [configId, setConfigId] = useState<string | null>(null);
+    const [shouldRedirect, setShouldRedirect] = useState<boolean>(false);
+
+    const router = useRouter();
+
+    useEffect(() => {
+        const configurationId = localStorage.getItem("configurationId");
+        if(configurationId) setConfigId(configurationId);
+    },[])
+
+    const { data } = useQuery({
+        queryKey: ["auth-callback"],
+        queryFn: async () => await getAuthStatus(),
+        retry: true,
+        retryDelay: 500,
+    })
+
+    useEffect(() => {
+        if(data?.success && configId) {
+            localStorage.removeItem("configurationId");
+            setShouldRedirect(true);
+        }
+    }, [data, configId]);
+
+    useEffect(() => {
+        if(shouldRedirect) {
+            router.push(`/configure/preview?id=${configId}`);
+        }
+    }, [shouldRedirect]);
+
+    if(data?.success && !configId) {
+        setShouldRedirect(true);
+    }
+
+    return (
+        <div className='w-full mt-24 flex justify-center'>
+          <div className='flex flex-col items-center gap-2'>
+            <Loader2 className='h-8 w-8 animate-spin text-zinc-500' />
+            <h3 className='font-semibold text-xl'>Logging you in...</h3>
+            <p>You will be redirected automatically.</p>
+          </div>
+        </div>
+    );
+}
+
+export default Page;
